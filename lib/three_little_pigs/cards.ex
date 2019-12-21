914aa350286e986721d14df7f0ba82f43d874e25
Santiago Bacaro <sbacarob@outlook.com>
2019-12-21 22:57:03 +0100

    add logic modules

---
+++ lib/three_little_pigs/cards.ex
@@ -0,0 +1,42 @@
+defmodule ThreeLittlePigs.Cards do
+  @moduledoc """
+  Contains an API for creating, retrieving and deleting cards
+  """
+
+  alias ThreeLittlePigs.{Card, Repo}
+
+  @doc """
+  Create a card with the given attributes
+  """
+  def create_card(attrs) do
+    %Card{}
+    |> Card.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Get a card by id
+  """
+  def get_card(id) do
+    Repo.get!(Card, id)
+  end
+
+  @doc """
+  Delete a card by id
+  """
+  def delete_card(id) do
+    id
+    |> get_card()
+    |> Repo.delete()
+  end
+
+  @doc """
+  Edit a card with the given attributes
+  """
+  def edit_card(id, attrs) do
+    id
+    |> get_card()
+    |> Card.changeset(attrs)
+    |> Repo.update()
+  end
+end
