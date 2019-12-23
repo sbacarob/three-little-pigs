98c5f810b0dc146ea7f4314ef1beff02c96e0489
Santiago Bacaro <sbacarob@outlook.com>
2019-12-23 23:57:58 +0100

    add meetings controller and routes

--- lib/three_little_pigs_web/router.ex
+++ lib/three_little_pigs_web/router.ex
@@ -18,6 +18,9 @@ defmodule ThreeLittlePigsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    resources "/meetings", MeetingController, only: [:show, :update], param: "uuid"
+    post "/meetings", MeetingController, :create
   end
 
   # Other scopes may use custom stacks.
