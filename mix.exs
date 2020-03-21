979d52ec9312d5e96f4380724373d1e1306b4b71
Santiago Bacaro <sbacarob@outlook.com>
2020-03-21 14:40:21 +0100

    update elixir version and some styles

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ThreeLittlePigs.MixProject do
     [
       app: :three_little_pigs,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
